3603e56562158f5cdcc3ec2847f39c6a2468029e
akaunderr <akaunderr@gmail.com>
2024-06-14 13:08:35 +0300

    updated module wrapper

--- ocmodw.ini.php
+++ ocmodw.ini.php
@@ -6,7 +6,7 @@ define('YEAR', '2023');
 define('AUTHOR', 'Andrii Burkatskyi');
 define('EMAIL', 'ocmod.space@gmail.com');
 
-define('MODLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=45444');
+define('MODLINK', 'https://www.ocmod.space/seo-common-urls');
 define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=45444');
 define('GITLINK', 'https://github.com/ocmod-space/ocmod-seo-common-urls');
 define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
